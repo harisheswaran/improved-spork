983cd823a51ceb34db925a3b6bef3d4df438d140
Harish E <137715353+harisheswaran@users.noreply.github.com>
2023-10-08 06:18:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Example: Add a simple function for form submission
+document.querySelector('#contact form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    // Process the form data here or display a thank-you message
+    alert('Thank you for contacting me!');
+});
